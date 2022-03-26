56198cae0011fe9e8bec53b82cb881c4d5105ee0
Cody <cody@worsnop.com>
2022-03-26 14:30:01 -0700

    Various misc fixes

--- TLTDatabaseEditor/Properties/Settings.Designer.cs
+++ TLTDatabaseEditor/Properties/Settings.Designer.cs
@@ -26,8 +26,7 @@ namespace TLTDatabaseEditor.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=Coruscant;Initial Catalog=TLT_Inventory;Persist Security Info=True;Us" +
-            "er ID=tltinvfull;Password=z3z3gu8EmecEPhU-aGexuq+!")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=localhost\\SQLEXPRESS;Database=master2;Trusted_Connection=True;")]
         public string TLT_InventoryConnectionString {
             get {
                 return ((string)(this["TLT_InventoryConnectionString"]));
